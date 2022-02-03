eea8e153f86c7a4cb2e4bf88686571a83cd0628a
Alexander Aranda <Alexandermarkets1@gmail.com>
2022-02-03 02:35:18 -0800

    three-js-spriral: ready to launch

--- src/script.js
+++ src/script.js
@@ -139,8 +139,8 @@ const tick = () => {
 
 
     if (mouseX > 0) {
-        particlesMesh.rotation.x = -mouseY * (elapsedTime * .000025)
-        particlesMesh.rotation.y = -mouseX * (elapsedTime * .000025)
+        particlesMesh.rotation.x = -mouseY * (elapsedTime * .000050)
+        particlesMesh.rotation.y = -mouseX * (elapsedTime * .000050)
     }
     // Update Orbital Controls
     // controls.update()
